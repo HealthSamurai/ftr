721b32fae9a361f5c6d0199ae3bdd7c47bccd0db
Oleg Veschin <veschindev@gmail.com>
2022-09-15 16:35:22 +0300

    add migration test
    Co-authored-by: @ghrpp <profunctorlense@gmail.com>

--- test/ftr/core_test.clj
+++ test/ftr/core_test.clj
@@ -489,3 +489,48 @@
 
   (ftr.utils.core/rmrf (:ftr-path ig-test-env-cfg))
   )
+
+(t/deftest migrate-test
+  (let [
+        env        {:ig-source-initial  "test/fixture/dehydrated.core/node_modules"
+                    :ig-source-updated1 "test/fixture/dehydrated.mutated.core/node_modules"
+                    :ig-source-updated2 "test/fixture/dehydrated.mutated.core2/node_modules"
+                    :update-plan-path   "/tmp/igftr/update-plan.ndjson.gz"
+                    :ftr-path           "/tmp/igftr"}
+        user-cfg   {:module      "dehydrated"
+                    :source-url  (:ig-source-initial env)
+                    :ftr-path    (:ftr-path env)
+                    :tag         "v1"
+                    :source-type :ig}
+        _          (sut/apply-cfg user-cfg)
+        client-cfg (merge user-cfg
+                          {:tag "v2"
+                           :move-tag {:old-tag "v1"
+                                      :new-tag "v2"}
+                           :tag-index (slurp (format "%s/%s/tags/%s.ndjson.gz"
+                                                     (:ftr-path env)
+                                                     (:module user-cfg)
+                                                     (:tag user-cfg)))})
+
+        ]
+
+    (sut/apply-cfg (assoc user-cfg :source-url (:ig-source-updated1 env)))
+
+    (sut/apply-cfg (merge user-cfg {:source-url (:ig-source-updated2 env)
+                                    :tag        "v2"
+                                    :move-tag   {:old-tag "v1"
+                                                 :new-tag "v2"}}))
+
+
+    #_(t/testing "migrate"
+        (matcho/match
+         (pull-sut/migrate client-cfg))
+        {:update-plan (:update-plan-path env)
+         :remove-plan ()}
+        )
+
+
+    )
+  
+  ;;
+  )
