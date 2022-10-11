ef4343058cf2d272e9cd62f3c65d44067cb2b235
KGOH <kgofhedgehogs@gmail.com>
2022-10-11 12:32:07 +0200

    Add ftr.zen-package-test ftr fixture
    Co-authored-by: @ghrpp <profunctorlense@gmail.com>

--- test/ftr/zen_package_test.clj
+++ test/ftr/zen_package_test.clj
@@ -54,8 +54,17 @@
   (spit (str module-dir-path "/zen-package.edn")
         {:deps (into {}
                      (map (fn [dep-name]
-                            [dep-name (mk-module-dir-path root-dir-path dep-name)]))
+                            (if (vector? dep-name)
+                              [(first dep-name) (second dep-name)]
+                              [dep-name (mk-module-dir-path root-dir-path dep-name)])))
                      deps)}))
+
+
+(defn spit-resources [root-dir-path module-dir-path resources]
+  (mkdir (str module-dir-path "/resources/"))
+  (doseq [[path content] resources]
+    (spit (str module-dir-path "/resources/" path)
+          content)))
 
 
 (defn mk-module-fixture [root-dir-path module-name module-params]
@@ -69,6 +78,8 @@
 
     (spit-deps root-dir-path module-dir-path (:deps module-params))
 
+    (spit-resources root-dir-path module-dir-path (:resources module-params))
+
     (git-init-commit module-dir-path)
 
     :done))
@@ -77,44 +88,68 @@
 (defn mk-fixtures [test-dir-path deps]
   (mkdir test-dir-path)
 
-  (doseq [module-name (keys deps)]
-    (mk-module-fixture test-dir-path module-name (get deps module-name))))
+  (doseq [[module-name module-params] deps]
+    (mk-module-fixture test-dir-path module-name module-params)))
 
 
 (defn rm-fixtures [test-dir-path]
   (rm test-dir-path))
 
 
-(def test-zen-repos
-  {'test-module {:deps '#{a-lib}
-                 :zrc '#{{:ns main
-                          :import #{a}
-                          sym {:zen/tags #{a/tag}
-                               :a "a"}}}}
+(def icd10-no-header-csv-content
+  "10344;20;XX;External causes of morbidity and mortality;;;1;
+16003;2001;V01-X59;Accidents;10344;;1;
+15062;20012;W00-X59;Other external causes of accidental injury;16003;;1;10/07/2020
+11748;2001203;W50-W64;Exposure to animate mechanical forces;15062;;1;
+11870;2001203W64;W64;Exposure to other and unspecified animate mechanical forces;11748;;1;
+11871;2001203W640;W64.0;Exposure to other and unspecified animate mechanical forces home while engaged in sports activity;11870;;1;
+11872;2001203W641;W64.00;Exposure to other and unspecified animate mechanical forces, home, while engaged in sports activity;11871;;1;
+11873;2001203W641;W64.01;Exposure to other and unspecified animate mechanical forces, home, while engaged in leisure activity;11871;;1;")
 
-   'a-lib       {:deps '#{a-lib-dep b-lib}
-                 :zrc '#{{:ns a
-                          :import #{a-dep b}
-                          tag {:zen/tags #{zen/schema zen/tag}
-                               :confirms #{a-dep/tag-sch}}
-                          recur-sch {:zen/tags #{zen/schema}
-                                     :type zen/map
-                                     :keys {:a {:confirms #{b/recur-sch}}}}}}}
 
-   'b-lib        {:deps '#{a-lib}
-                  :zrc '#{{:ns b
-                           :import #{a}
-                           recur-sch {:zen/tags #{zen/schema}
-                                      :type zen/map
-                                      :keys {:b {:confirms #{a/recur-sch}}}}}}}
+(defn test-zen-repos [root-path]
+  {'profile-lib {:deps #{['zen-fhir (str (System/getProperty "user.dir") "/zen.fhir/")]}
+                 :resources {"icd-10.csv" icd10-no-header-csv-content}
+                 :zrc #{{:ns 'profile
+                         :import #{'zen.fhir}
 
-   'a-lib-dep   {:deps '#{}
-                 :zrc '#{{:ns a-dep
-                          tag-sch
-                          {:zen/tags #{zen/schema}
+                         'diagnosis-vs
+                         {:zen/tags #{'zen.fhir/value-set}
+                          :zen.fhir/version "0.5.0"
+                          :uri "diagnosis-vs"
+                          :ftr {:module            "ftr"
+                                :source-url        (str root-path "/profile-lib/resources/icd-10.csv")
+                                :ftr-path          root-path
+                                :tag               "v1"
+                                :source-type       :flat-table
+                                :extractor-options {:format "csv"
+                                                    :csv-format {:delimiter ";"
+                                                                 :quote     "'"}
+                                                    :header      false
+                                                    :data-row    0
+                                                    :mapping     {:concept {:code    {:column 2}
+                                                                            :display {:column 3}}}
+                                                    :code-system {:id  "icd10"
+                                                                  :url "http://hl7.org/fhir/sid/icd-10"}
+                                                    :value-set   {:id   "icd10"
+                                                                  :name "icd10.accidents"
+                                                                  :url  "http://hl7.org/fhir/ValueSet/icd-10"}}}}
+
+                         'sch
+                         '{:zen/tags #{zen/schema zen.fhir/structure-schema}
+                           :zen.fhir/version "0.5.0"
                            :type zen/map
-                           :require #{:a}
-                           :keys {:a {:type zen/string}}}}}}})
+                           :keys {:diagnosis {:type zen/string
+                                              :zen.fhir/value-set {:symbol diagnosis-vs}}}}}}}
+
+   'test-module {:deps '#{profile-lib}
+                 :zrc '#{{:ns main
+                          :import #{profile}
+
+                          sch {:zen/tags #{zen/schema}
+                               :confirms #{profile/sch}
+                               :type zen/map
+                               :require #{:diagnosis}}}}}})
 
 
 (t/deftest init-test
@@ -122,7 +157,7 @@
 
   (rm-fixtures test-dir-path)
 
-  (mk-fixtures test-dir-path test-zen-repos)
+  (mk-fixtures test-dir-path (test-zen-repos test-dir-path))
 
   (def module-dir-path (str test-dir-path "/test-module"))
 
@@ -134,4 +169,3 @@
 
   (t/testing "no errors in package"
     (t/is (empty? (zen.core/errors ztx)))))
-
